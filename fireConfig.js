9cfef16a97ebba09540948adef04b75a34d31acd
hosrph <joseph.amargo@lpunetwork.edu.ph>
2025-05-20 12:34:10 +0800

    Update fireConfig.js

--- fireConfig.js
+++ fireConfig.js
@@ -1,11 +1,11 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyDJHqqtCRBOmGXY4mBE01iAyVKDPpCVBNk",
-  authDomain: "crud-c86bf.firebaseapp.com",
-  databaseURL: "https://crud-c86bf-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "crud-c86bf",
-  storageBucket: "crud-c86bf.firebasestorage.app",
-  messagingSenderId: "1013611005058",
-  appId: "1:1013611005058:web:b8731232adf2aed73e209b"
+  apiKey: "AIzaSyAfwd7htKlzNUIO9msKgmU1f9DeVJJ7JoQ",
+  authDomain: "crud2-d32c4.firebaseapp.com",
+  databaseURL: "https://crud2-d32c4-default-rtdb.firebaseio.com",
+  projectId: "crud2-d32c4",
+  storageBucket: "crud2-d32c4.appspot.com",
+  messagingSenderId: "1059787362344",
+  appId: "1:1059787362344:web:8e4d139a13ec372e88651d"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
